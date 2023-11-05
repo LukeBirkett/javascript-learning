eec657063cfe6e32e4df0d571cc011ba32ae0f8d
luke_birkett <contactbirkett@gmail.com>
2023-11-05 18:02:50 +0900

    qa js finished

--- js-build-basic/q_and_a/app.js
+++ js-build-basic/q_and_a/app.js
@@ -1,2 +1,27 @@
-//using selectors inside the element
-// traversing the dom
+// Option 1: traversing the dom (moving up and down dom tree)
+// const btns = document.querySelectorAll('.question-btn')
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function (e) {
+//         // console.log(e.currentTarget);
+//         // console.log(e.currentTarget.parentElement.parentElement);
+//         const question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle('show-text')
+//     });
+// });
+
+// Option 2: using selectors inside the element
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function () {
+        questions.forEach(function(item){
+            console.log(item)
+            if(item !== question){
+                item.classList.remove('show-text');
+            }
+        });
+        question.classList.toggle('show-text');
+    });
+});
