df3f10f45eb7fe27103dbc0447c663962b520489
luke_birkett <contactbirkett@gmail.com>
2024-01-15 16:40:37 +0000

    task manager start

--- node-express-course/03-task-manager/starter/app.js
+++ node-express-course/03-task-manager/starter/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks.js')
+
+// routes
+app.get('/', (req,res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 4501
+
+app.listen(port, console.log(`Server is Listening on port ${port}...`))
+
+
