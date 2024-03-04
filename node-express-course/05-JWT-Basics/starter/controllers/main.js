e9700237ac2ea280bacfedeef30c6d46133af89e
luke_birkett <contactbirkett@gmail.com>
2024-03-04 18:28:37 +0530

    jwt started

---
+++ node-express-course/05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
+const login = async (req,res) => {
+    const {username, password} = req.body
+
+    if (!username || !password){
+        throw new CustomAPIError("please provide email and passowrd", 400) 
+    }
+
+    const id = new Date().getDate()
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'})
+
+    res.status(200).json({msg:'user created', token})
+}
+
+const dashboard = async (req,res) => {
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({
+        msg: `hello, john doe`, 
+        secret: `here is auth data, lucky num is ${luckyNumber}`
+    })
+}
+
+module.exports = {
+    login, dashboard
+}
