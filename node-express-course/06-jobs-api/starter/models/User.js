5214e722deac9de995c48a3be03595f345a2d066
luke_birkett <contactbirkett@gmail.com>
2024-03-08 16:01:30 +0530

    hashing middleware

--- node-express-course/06-jobs-api/starter/models/User.js
+++ node-express-course/06-jobs-api/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -18,10 +19,15 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: [true, 'please prov valid name'],
-        minlength: 6,
-        maxlength: 12
+        minlength: 6
     },
 
+})
+
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 module.exports = mongoose.model('User', userSchema)
